768b36c613399d6f309dae22fdf9886e3040a215
kirito <kiritoxjf@outlook.com>
2024-05-26 20:06:35 +0800

    PostCSS插件补充

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,10 +13,6 @@ export default {
       gray: '#8492a6',
       'gray-light': '#d3dce6'
     },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif']
-    },
     extend: {
       spacing: {
         '8xl': '96rem',
